e805eda9279a33e35fbaf9da3ca6333dce81cb5d
Brent Perry <burentoperii@gmail.com>
2016-07-13 11:32:33 -0500

    Initial commit of FirstNOfFibonacci.

--- FirstNOfFibonacci/main.cpp
+++ FirstNOfFibonacci/main.cpp
@@ -6,10 +6,32 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    vector<int> fibonacci;
+    int n;
+    
+    cout << "Please enter how many numbers of the Fibonacci series you want to see.\n";
+    cout << "Must be at least 3 (and more than 46 won't print): ";
+    cin >> n;
+    cout << '\n';
+    
+    if (n > 2) {
+        fibonacci.push_back(1);
+        fibonacci.push_back(1);
+        for (int i = 2; i < n; ++i) {
+            fibonacci.push_back(fibonacci[i - 2] + fibonacci[i - 1]);
+        }
+        
+        cout << "Here are the first " << n << " numbers of the Fibonacci series: \n";
+        for (int i = 0; i < fibonacci.size(); ++i) {
+            cout << fibonacci[i] << " ";
+        }
+        cout << '\n';
+    } else {
+        cout << "No, really, it's pretty pointless if you don't enter at least '3'.\n";
+    }
+    
 }
